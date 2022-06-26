4bb8c3cfcf878422f4d068bd28543ec475c80c69
현경 김 <ghwls9117@naver.com>
2022-06-26 21:53:41 +0900

    [Conflict] Test login API

--- back/routes/api/users/index.js
+++ back/routes/api/users/index.js
@@ -5,7 +5,6 @@ const { User, AuthToken, Post } = require('../../../models'); // db.User, db.Aut
 
 const router = express.Router();
 
-<<<<<<< HEAD
 // 로그인 유저
 router.get('/me', (req, res, next) => {
 	console.log(req.user);
@@ -38,51 +37,6 @@ router.post('/', async (req, res, next) => {
 		console.error(error);
 		next(error);
 	}
-=======
-// api/users/
-// 로그인 유저 
-router.get("/me", (req, res, next) => {
-  console.log(req.user);
-  return res.json(req.user || false);
-});
-
-// 유저 생성
-router.post("/", async (req, res, next) => {
-  const { auth, name, nickname, password } = req.body;
-  let email = null;
-  let phone = null;
-  if (auth.indexOf("@") > -1) {
-    email = auth;
-  } else {
-    phone = auth;
-  }
-
-  req.session.prepUser = { email, phone, name, nickname, password };
-
-  try {
-    const existingUser = await User.findOne({ where: { nickname } });
-    if (existingUser) {
-      return res.status(403).send("이미 사용중인 닉네임입니다."); //403 - 클라이언트 오류 & 금지
-    }
-    const payload = Math.floor(100000 + Math.random() * 900000) + "";
-    const authToken = await AuthToken.create({ payload });
-    console.log(authToken);
-    // return res.status(200).json({ email, phone, nickname });
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const newUser = await User.create({
-      email,
-      phone,
-      name,
-      nickname,
-      password: hashedPassword,
-    });
-    return res.status(201).send("ok"); // 200 - 성공, 201 - 생성
-
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
->>>>>>> HG
 });
 
 // 유저 이메일, 전화번호 인증
@@ -114,7 +68,6 @@ router.post('/confirm', async (req, res, next) => {
 
 // 로그인
 
-<<<<<<< HEAD
 router.post('/login', (req, res, next) => {
 	passport.authenticate('local', (err, user, info) => {
 		// 서버 에러
@@ -166,60 +119,6 @@ router.post('/logout', (req, res, next) => {
 		req.session.destroy();
 		return res.send('ok');
 	});
-=======
-router.post("/login", (req, res, next) => {
-  passport.authenticate("local", (err, user, info) => {
-    // 서버 에러
-    if (err) {
-      console.error(err);
-      return next(err);
-    }
-
-    // 클라이언트 에러
-    if (info) {
-      return res.status(401).send(info.reason); // 401 - 허가되지 않음
-    }
-      console.log(user);
-    return req.login(user, async (loginErr) => {
-      if (loginErr) {
-        console.error(loginErr);
-        return next(loginErr);
-      }
-
-      const fullUserWithoutPassword = await User.findOne({
-        where: { id: user.id },
-        attributes: {
-          exclude: ["password"],
-        },
-        include: [
-          {
-            model: Post,
-          },
-          {
-            model: User,
-            as: "Followers",
-          },
-          {
-            model: User,
-            as: "Followings",
-          },
-        ],
-      });
-
-      console.log(fullUserWithoutPassword);
-      return res.status(200).json(fullUserWithoutPassword); // 성공 시 사용자 정보 넘겨주기.
-    });
-  })(req, res, next);
-});
-
-router.post("/logout", (req, res) => {
-  req.logout((err) => { 
-    if(err) {console.error(err)}
-    req.session.destroy(); 
-    return res.send("ok");
-  }); 
- 
->>>>>>> HG
 });
 
 module.exports = router;
